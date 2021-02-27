b878c72aeecf3f214d3c22d5cffa911e2f3dd7bf
santiadlv <49079265+santiadlv@users.noreply.github.com>
2021-02-26 20:41:55 -0500

    Add numbers to screen

--- calculator/calculator/ViewController.swift
+++ calculator/calculator/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func processNumber(_ sender: UIButton) {
+        if let numberText = sender.titleLabel?.text {
+            if (displayLabel.text == "0") {
+                displayLabel.text = numberText
+            } else {
+                displayLabel.text = displayLabel.text! + numberText
+            }
+        }
+    }
 
+    
 }
 
