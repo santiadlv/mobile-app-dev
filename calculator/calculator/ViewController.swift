808e38df47fa2125e7734c151644e7d7c04b5a0e
santiadlv <49079265+santiadlv@users.noreply.github.com>
2021-03-03 02:43:07 -0500

    Decimal, sign change and clear buttons implemented

--- calculator/calculator/ViewController.swift
+++ calculator/calculator/ViewController.swift
@@ -11,6 +11,17 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var displayLabel: UILabel!
     
+    var current = ""
+    var leftOperator: Double?
+    var rightOperator: Double?
+    var operation = ""
+    var result: Double?
+    
+    var canTakeDecimal: Bool = true
+    var numIsPositive: Bool = true
+    var clearDisplayLabel: Bool = false
+    var clearOnEquals: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,14 +29,75 @@ class ViewController: UIViewController {
 
     @IBAction func processNumber(_ sender: UIButton) {
         if let numberText = sender.titleLabel?.text {
-            if (displayLabel.text == "0") {
-                displayLabel.text = numberText
+            if !clearDisplayLabel {
+                if (displayLabel.text == "0") {
+                    displayLabel.text = numberText
+                } else {
+                    displayLabel.text = displayLabel.text! + numberText
+                }
             } else {
-                displayLabel.text = displayLabel.text! + numberText
+                displayLabel.text = numberText
+                clearDisplayLabel = false
             }
         }
     }
-
+    
+    @IBAction func processDecimal(_ sender: UIButton) {
+        guard canTakeDecimal else {
+            return
+        }
+        
+        if clearOnEquals {
+            reset()
+        }
+        displayLabel.text = displayLabel.text! + "."
+        canTakeDecimal = false
+    }
+    
+    @IBAction func processOperation(_ sender: UIButton) {
+        if let operationText = sender.titleLabel?.text {
+            if operation.isEmpty {
+                operation = operationText
+                current = displayLabel.text ?? "0"
+                clearDisplayLabel = true
+            } else {
+                
+            }
+        }
+    }
+    
+    @IBAction func processClear(_ sender: UIButton) {
+        reset()
+    }
+    
+    @IBAction func processSignChange(_ sender: UIButton) {
+        if numIsPositive {
+            displayLabel.text = "-" + displayLabel.text!
+            numIsPositive = false
+        } else {
+            displayLabel.text?.remove(at: displayLabel.text!.startIndex)
+            numIsPositive = true
+        }
+    }
+    
+    func saveNums() {
+        if leftOperator == nil {
+            leftOperator = Double(current)
+        } else if (rightOperator == nil) {
+            rightOperator = Double(current)
+        }
+    }
+    
+    func reset() {
+        displayLabel.text = "0"
+        current = ""
+        leftOperator = nil
+        rightOperator = nil
+        result = nil
+        canTakeDecimal = true
+        clearDisplayLabel = false
+        clearOnEquals = false
+    }
     
 }
 
