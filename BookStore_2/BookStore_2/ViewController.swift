9d6cbc66daecf6ae46a081187c01f2aac5a04797
santiadlv <49079265+santiadlv@users.noreply.github.com>
2021-04-09 21:16:18 -0400

    Committing Bookstore 2 App

---
+++ BookStore_2/BookStore_2/ViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  BookStore_2
+//
+//  Created by user192416 on 4/6/21.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var myTableView: UITableView!
+    var managedObjectContext: NSManagedObjectContext!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let appDelegate: AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        managedObjectContext = appDelegate.persistentContainer.viewContext as NSManagedObjectContext
+    }
+    
+    func loadBooks() -> [Book] {
+        let fetchRequest: NSFetchRequest<Book> = Book.fetchRequest()
+        let titleSort: NSSortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+        fetchRequest.sortDescriptors = [titleSort]
+        var result: [Book] = []
+        
+        do {
+            result = try managedObjectContext.fetch(fetchRequest)
+        } catch {
+            NSLog("My error: %@", error as NSError)
+        }
+        
+        return result
+    }
+
+
+    @IBAction func addNew(_ sender: UIBarButtonItem) {
+        let book: Book = NSEntityDescription.insertNewObject(forEntityName: "Book", into: managedObjectContext) as! Book
+        book.title = "My Book" + String(loadBooks().count)
+        
+        do {
+            try managedObjectContext.save()
+        } catch let error as NSError {
+            NSLog("My error: %@", error)
+        }
+        
+        myTableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return loadBooks().count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        else {
+            return UITableViewCell()
+        }
+        
+        let book: Book = loadBooks()[indexPath.row]
+        cell.textLabel?.text = book.title
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete) {
+            let book: Book = loadBooks()[indexPath.row]
+            managedObjectContext.delete(book)
+            do {
+                try managedObjectContext.save()
+            } catch let error as NSError {
+                NSLog("My error: %@", error)
+            }
+            myTableView.reloadData()
+        }
+    }
+}
+
